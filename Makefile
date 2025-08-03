3ebc14325774793c456ce50321ac81a98dca2535
Matheus Garcia <37114863+Krush206@users.noreply.github.com>
2025-08-03 13:35:36 -0300

    General update

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
+include common.mk
+
+LIBS = ./lib/lib.o ./linux/linux.o ./objects/objects.o
+
 all:
-	$(MAKE) -C lib
-	$(MAKE) -C linux
-	$(MAKE) -C objects
-	ar q libhotdog.a lib/*.o objects/*.o linux/*.o
+	$(MAKE) -C ./lib
+	$(MAKE) -C ./linux
+	$(MAKE) -C ./objects
+	ar q ./libHOTDOG.a $(LIBS)
 
 clean:
-	$(MAKE) -C lib clean
-	$(MAKE) -C linux clean
-	$(MAKE) -C objects clean
-	rm -f ./libhotdog.a
+	$(MAKE) -C ./lib clean
+	$(MAKE) -C ./linux clean
+	$(MAKE) -C ./objects clean
+	rm -f ./libHOTDOG.a
--- include/HOTDOG.h
+++ include/HOTDOG.h
@@ -182,6 +182,368 @@ typedef struct {
 @end
 @interface Definitions : NSObject
 @end
+@interface Definitions(Fjewilfmlkdsmvlksdkjf)
+@end
+@interface Definitions(Fjewilfmlkdsmvlksdkjffjdksljfkldsjkffjdskffsdjkfjsdkjk)
+@end
+@interface Definitions(Fjewilfmlkdsmvlksdkjffjdksljfkldsjkffjdskfjk)
+@end
+@interface Definitions(Fjewilfmlkdsmvlksdkjffjdksljfkldsjkffjdskfjkfjdskfjsdk)
+@end
+@interface Definitions(Fjewilfmlkdsmvlksdkjffjdksljfkldsjkffjdskfjkfjdskfjsdkfjdksjfks)
+@end
+@interface Definitions(Fjewilfmlkdsmvlksdkjffjdksljfkldsjkffjdskfjkjfkdsjkfj)
+@end
+@interface Definitions(Fjewilfmlkdsmvlksdkjffjkjekwljfklwe)
+@end
+@interface Definitions(INMfefdisfjwlfmklsdmvklsjdklfjklsdffjdkslmfklxcmvklcfdsmkfmekkfxdsfmneoiiooikl)
+@end
+@interface Definitions(INMfewlfmklsdmvklsjdklfjklsdffjdkslmfklxcmvklcfdsmkfmekkfxdsfmneoiiooikl)
+@end
+@interface Definitions(INMfewlfmklsdmvklsjdklfjklsdffjdkslmfklxcmvklcfdsmkfmekkfxkl)
+@end
+@interface Definitions(INMfewlfmklsdmvklsjdklfjklsdffjdkslmfklxcmvklcxkl)
+@end
+@interface Definitions(INMfewlfmklsdmvklsjdklfjklsdfjfksdjfkj)
+@end
+@interface Definitions(dsfjkdsjflkdsjlkfsdjf)
+@end
+@interface Definitions(emjfkwlmfkldlskfm)
+@end
+@interface Definitions(fdsjfklsdklfjklsdjfklsjdklfj)
+@end
+@interface Definitions(fewiomfiodsvmkocxjvoksjdfoks)
+@end
+@interface Definitions(fjdklsjfkldsjfsdifjikdiikjf)
+@end
+@interface Definitions(fjdklsjfkldsjklfjs)
+@end
+@interface Definitions(fjdklsjfkldskewiuriewurijf)
+@end
+@interface Definitions(fjdklsjfkldskjeuriefjdif)
+@end
+@interface Definitions(fjdklsjfkldskjeuriwef)
+@end
+@interface Definitions(fjdklsjfkldskjf)
+@end
+@interface Definitions(fjdklsjfkldskjfurijvicx)
+@end
+@interface Definitions(fjdklsjfkljfiewjfijidskjf)
+@end
+@interface Definitions(fjdklsjflksdjf)
+@end
+@interface Definitions(fjdkslfjklsdf)
+@end
+@interface Definitions(fjdkslfjklsdjf)
+@end
+@interface Definitions(fjdkslfjlkdslfjldksjfdsfioewjfiowe)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkejwklfkdsfmkf)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkf)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkfjdskfjksdf)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkfjdskfjksdffjdksjfk)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkfjdskfjksdffjsdkfjksdfjksd)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkfjdskfjksdfjfdksf)
+@end
+@interface Definitions(fjdksljfklsdjfdsjkfljdsklfjkfjdskfjksdfjkdj)
+@end
+@interface Definitions(fjeiowmfklsjvojiewjfksdjksjkdjfks)
+@end
+@interface Definitions(fjeiwofmkdsomvklcxjvlksjdfkjds)
+@end
+@interface Definitions(fjejfksdljfklsdjfkkwlfmkldsmfkldsjflkfjdskfjksd)
+@end
+@interface Definitions(fjejfksdljfklsdjfkkwlfmkldsmfkldsjflkfjdskfjksdfjdksfj)
+@end
+@interface Definitions(fjejfksdljfklsdjfkkwlfmkldsmfkldsjflkfjdskfjksdfjdksfjksd)
+@end
+@interface Definitions(fjejfksdljfklsdjfkkwlfmkldsmfkldsjflkfjdskfjksdfjdskfjk)
+@end
+@interface Definitions(fjejfksdljfklsdjfkkwlfmkldsmfkldsjflkfjdskfjksdfjksdjfksd)
+@end
+@interface Definitions(fjeklwjfklsdmklfmklsd)
+@end
+@interface Definitions(fjeklwjfklsmdklfmiewmoofmkdsf)
+@end
+@interface Definitions(fjeklwjflksdjf)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflfjdskfjkdsk)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjfljkjkjkjklsdfwk)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflk)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflkfjdskfjksd)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflkfjdskfjksdfjdksfjksd)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflkfjdskfjksdfjdksjfksd)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflkfjdskfjksdfjkdsjfk)
+@end
+@interface Definitions(fjekwlfmkldsmfkldsjflkfjdskfjksdfjsdkjfksd)
+@end
+@interface Definitions(fjekwlmfkldsmfklsdfm)
+@end
+@interface Definitions(fjewlfiewfmklsdjfklsdjf)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklfdsjfjdskfjk)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklfdsjfjdskfjkfjdksfjk)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklfdsjfjdskfjkfjksdjfklsdfj)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklfdsjfjdskfjkfjsdkf)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklfdsjfjdskfjkjfkdsjfks)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklk)
+@end
+@interface Definitions(fjewmfnkdslnfsdjffdsjkflsdmkmklkfjdksljfklsdj)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflfjdskfjkdsk)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflfjdskfjksldjfkk)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflfjdskfjksldjfkkfjdksfjksd)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflfjdskfjksldjfkkfjdksfjksdjf)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflfjdskfjksldjfkkfjdksjf)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflfjdskfjksldjfkkfjdksjffjdsk)
+@end
+@interface Definitions(fjewmfnkdslnfsdjflk)
+@end
+@interface Definitions(fjieowfjkldsjflksdjkfl)
+@end
+@interface Definitions(fjieowjfklsdjkflsdfj)
+@end
+@interface Definitions(fjilsjflksdjfklsdjklfj)
+@end
+@interface Definitions(fjkdeifjdclsjfiowejfklsdjfklsdkljf)
+@end
+@interface Definitions(fjkdlsfjjfdksjfklksdejwklfmksldmfklsdmfklsdjkf)
+@end
+@interface Definitions(fjkdlsfjlksdejwklfmksldmfklsdmfklsdjkf)
+@end
+@interface Definitions(fjkdlsfjlksdjdskfldsjkfjfkdlsmfkldsmkfljkf)
+@end
+@interface Definitions(fjkdlsjfkldsjfkldsjklfwejffjdkjfkdjlsd)
+@end
+@interface Definitions(fjkdlsjfkldsjfkldsjklfwejffjdkjfkdjlsdfjdsk)
+@end
+@interface Definitions(fjkdlsjfkldsjflksdjfisdnmfilewv)
+@end
+@interface Definitions(fjkdlsjfklmnekwlvmlkdsjkvs)
+@end
+@interface Definitions(fjkdlsjfklmnekwlvmlkdsjkvsfjdskfjsdk)
+@end
+@interface Definitions(fjkdlsjfklmnekwlvmlkdsjkvsfjdskfjsdkfjdksjfks)
+@end
+@interface Definitions(fjkdlsjfklsdjfklsdfjdksjfkdsfjdskfjksdljfj)
+@end
+@interface Definitions(fjkdlsjfklsdjfklsdfjdksjfkdsfjdskfjksdljfjfdjskfjskd)
+@end
+@interface Definitions(fjkdlsjfklsdjfklsdfjdksjfkdsfjdskfjksdljfjfjdksfjksd)
+@end
+@interface Definitions(fjkdlsjfklsdjfklsdfjdksjfkdsfjdskfjksdljfjjfdksfjksd)
+@end
+@interface Definitions(fjkdlsjfklsdjfklsdfjdksjfkdsfjdskfjksdljfjjfdksjfksd)
+@end
+@interface Definitions(fjkdlsjfklsdjklfjsdf)
+@end
+@interface Definitions(fjkdlsjfklsdjklfsdklfj)
+@end
+@interface Definitions(fjkdlsjflksdjklfjfksdjf)
+@end
+@interface Definitions(fjkdlsjkfjkdfjlksdjdskfldsjkfjfkdlsmfkldsmkfljkf)
+@end
+@interface Definitions(fjkdsjfksdjkfjk)
+@end
+@interface Definitions(fjkdsjflkjdsklfj)
+@end
+@interface Definitions(fjkdslfjklsdjf)
+@end
+@interface Definitions(fjkdslfjklsdjflk)
+@end
+@interface Definitions(fjkdsljfkldsjlkfsdjf)
+@end
+@interface Definitions(fjkdsljfklsdjf)
+@end
+@interface Definitions(fjkdsljfklsdjfueirwieofj)
+@end
+@interface Definitions(fjkldsjflkdslkfj)
+@end
+@interface Definitions(fjkldsjlkfjdsf)
+@end
+@interface Definitions(fjklsdjfksdlkjfjskdlfjksdf)
+@end
+@interface Definitions(fjklwemklfmksdlvijsodjfksdfj)
+@end
+@interface Definitions(fjksdlfkjsdfj)
+@end
+@interface Definitions(fkelwmfklsdmklfmklsdmf)
+@end
+@interface Definitions(fmeilwfmdklsjfklsdjfkl)
+@end
+@interface Definitions(fmeiowfmkdsljvklxcmkljfkldfjkdsjfks)
+@end
+@interface Definitions(fmeiowfmkdsljvklxcmkljfklds)
+@end
+@interface Definitions(fmeiowmvkdsvijewi)
+@end
+@interface Definitions(fmeklwfmkldsjfmkldsklfkl)
+@end
+@interface Definitions(fmeklwfmklsdmfklsdkmf)
+@end
+@interface Definitions(fmeklwfmklsdmklfmsdkfl)
+@end
+@interface Definitions(fmeklwmfkdsmklf)
+@end
+@interface Definitions(fmekwlfeiosmfklxdvmosjdfiosdf)
+@end
+@interface Definitions(fmekwlfmksdlfmklsdkfm)
+@end
+@interface Definitions(fmekwlfmksdlfmklsdkfmfjdksfjkls)
+@end
+@interface Definitions(fmekwlfmksdlfmklsdkfmfjdksjfk)
+@end
+@interface Definitions(fmekwlfmksdlfmklsdkfmfjdksjfkfjdksfjsk)
+@end
+@interface Definitions(fmekwlfmksdlfmklsdkfmfjdksjfkfjdksfjskfjdksfj)
+@end
+@interface Definitions(fmekwlfmksdlfmklsdkfmfjdksjfkfjsdk)
+@end
+@interface Definitions(fmekwljfkdsjfksdmfklsdmfklsdmklfmlskdkf)
+@end
+@interface Definitions(fmekwljfkdsjfksdmfklsdmfklsdmklfmlskdkffjdskfjksdj)
+@end
+@interface Definitions(fmekwlmfklsdmfklsdmklfmlskdkf)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksld)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksldfjdksjffjdskfjsdkfjksk)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksldfjdksjfk)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksldfjdksjfkfjsdkffjdksfjksdk)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksldfjdksjfkfjsdkfk)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksldfjdksjfkfjsdkfkfjkd)
+@end
+@interface Definitions(fmekwlmfklsdmfklsmdklfmksldjfkdsjfk)
+@end
+@interface Definitions(fmekwlmfklsjfkdsjfdmfklsdmklfmlskdkf)
+@end
+@interface Definitions(fmkdlsmfklsdmklfmksdlmfklsdmf)
+@end
+@interface Definitions(fmkelwmfkldsmkf)
+@end
+@interface Definitions(fmkelwmfklsdklfmklsdmfklsdklfm)
+@end
+@interface Definitions(fmklsdfjklsdmfklsdlkf)
+@end
+@interface Definitions(fnmeklwfmklsdmfksdkfm)
+@end
+@interface Definitions(fnmjkdfsjkfsdjkeklwfmklsdmfksdkfm)
+@end
+@interface Definitions(fnmjkdfsjkfsdjkeklwfmklsdmfksdkfmdfjkjk)
+@end
+@interface Definitions(fnmjkdfsjkfsdjkeklwfmklsdmfksdkfmdfjkjkfjdksjf)
+@end
+@interface Definitions(gyubhjhghjkgyjghjghjkgybhbjbxdsdf)
+@end
+@interface Definitions(hkukgfdfthfnvbchjgfjygikghjghfjgfjdksfjksdkdjkfsdkjfj)
+@end
+@interface Definitions(hkukgfdfthfnvbchjgfjygikghjghfjgfjdksfjksdkdjkfsdkjfjdksfjkfj)
+@end
+@interface Definitions(hkukgfdfthfnvbchjgfjygikghjghfjgfjdksfjksdkjffjkdsjfksdj)
+@end
+@interface Definitions(hkukgfdfthfnvbchjgfjygikghjghfjgfjdksfjksdkjfj)
+@end
+@interface Definitions(hkukgfdfthfnvbchjgfjygikghjghfjgj)
+@end
+@interface Definitions(iewofkdslmvkcxvbapdwppqppowdsdkskskskaj)
+@end
+@interface Definitions(ifjeowjfkldsjfkldsjkflsjdlkfjdifieqf)
+@end
+@interface Definitions(ifjeowjfkldsjfkldsjkflsjdlkfjdifieqfdsjfksdjkf)
+@end
+@interface Definitions(jefklwmfkldmsklfmklsdf)
+@end
+@interface Definitions(jeklwjfkldmsklfmksdfmk)
+@end
+@interface Definitions(jfdosjfkldsjfklsdjklfjk)
+@end
+@interface Definitions(jfeklsfmlksdmvioewjfklds)
+@end
+@interface Definitions(jfelwmfkldsmfklmdsklfjiew)
+@end
+@interface Definitions(jfeokwfmklsdmfldjflksdjkf)
+@end
+@interface Definitions(jfewjfsdjflksdjfkljsdklf)
+@end
+@interface Definitions(jfkdlsjfkldskfj)
+@end
+@interface Definitions(jfkdlsjfklsdjf)
+@end
+@interface Definitions(jfkdlsjfklsdjfklsf)
+@end
+@interface Definitions(jfkdlsjfklsdjlkfjdsklfjlkdsfjkldsfewrwerwe)
+@end
+@interface Definitions(jfkdlsjflkdsjkfljdsf)
+@end
+@interface Definitions(jfkdlsjklfjsdklfljksdf)
+@end
+@interface Definitions(jfkelwmfklsmdklfmklsdfmksd)
+@end
+@interface Definitions(jfkldsjfkldsjlkfj)
+@end
+@interface Definitions(jfkldsjfklsdjf)
+@end
+@interface Definitions(jfkldsjklfjdsklfjlksdklfj)
+@end
+@interface Definitions(jfkldsjklfjslkdflksdjfklsdf)
+@end
+@interface Definitions(jfkldsjlkfsjffjdkslfjdsklfjskdd)
+@end
+@interface Definitions(jfoiwcciiejfklsdjfklsdjlkfjsdlkfj)
+@end
+@interface Definitions(jfoiwejfklsdjfklsdjlkfjsdejwfjkdlkfj)
+@end
+@interface Definitions(jfoiwejfklsdjfklsdjlkfjsdlkfj)
+@end
+@interface Definitions(jfoiwernfjejfklsdjfklsdjlkfjsdlkfj)
+@end
+@interface Definitions(jfoiwernfjejfklsdjfklsdjlkfjsdlkfjfdjskf)
+@end
+@interface Definitions(jfovcnvieiwejfklsdjfklsdjlkfjsdlkffdjskfjklsdjfklj)
+@end
+@interface Definitions(jfovcnvieiwejfklsdjfklsdjlkfjsdlkffdjskfjklsdjfkljjkjksdfjksdjj)
+@end
+@interface Definitions(jfovcnvieiwejfklsdjfklsdjlkfjsdlkfj)
+@end
+@interface Definitions(jfovcnvieiwejfklsdjfklsdjlkfjsdlkfjjfkdjsfksj)
+@end
+@interface Definitions(jfovcnvieiwejfklsdjfklsdjlkfjsdlkfjjfkdjsfksjfjdksfjksd)
+@end
+@interface Definitions(jklmklsafmklasdmflkasdmklfm)
+@end
+@interface Definitions(mfkelwmfklsdmklfm)
+@end
+@interface Definitions(mifomwieofmkdsjfiosdjfios)
+@end
+@interface Definitions(nfejklwnfklsdfmk)
+@end
+@interface Definitions(qiejfdkirekdfjivmiewfiefjkdfj)
+@end
 
 #import "HOTDOG-lib.h"
 #import "HOTDOG-objects.h"
